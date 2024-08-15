f762e4fa8a3a572bd10b0eb6d442e5601da793c1
Jorge Luis Moreno <84295095+jorg1230@users.noreply.github.com>
2024-08-15 15:47:58 -0500

    08/15/2024
    Conexión de graficos co la base de datos y actualizacion del modelo

--- SenaPlanning/SenaPlanning/Controllers/HomeController.cs
+++ SenaPlanning/SenaPlanning/Controllers/HomeController.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using ClaseModelo;
 
 namespace SenaPlanning.Controllers
 {
     public class HomeController : Controller
     {
+        private SenaPlanningEntities db = new SenaPlanningEntities();
+
         public ActionResult Index()
         {
-            return View();
+            return View(db.Meta.ToList());
         }
 
         public ActionResult About()
